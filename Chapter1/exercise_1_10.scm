daa0bb7694b6ca014c1a28b4d5bb8d339060cfe2
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2013-12-26 19:10:30 +0530

    Exercise 1.10

---
+++ Chapter1/exercise_1_10.scm
@@ -0,0 +1,19 @@
+;Exercise 1.10
+;
+;Values for the given procedure definition
+;
+;(A 1 10) = 1024
+;(A 2 4)  = 65536
+;(A 3 3)  = 65536
+;
+;
+;
+;
+;Mathematical definitions for the given functions
+;
+;(f n) = 2n
+;(g n) = 2^n
+;
+;(h n) = 2^[h(n-1)] where (h 0) = 0
+;                         (h 1) = 2
+;
