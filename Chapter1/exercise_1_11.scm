94d3c600ae5c83b01622f72ded8298d5144f348f
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2013-12-27 18:49:30 +0530

    Added Iterative procedure to Exercise 1.11

--- Chapter1/exercise_1_11.scm
+++ Chapter1/exercise_1_11.scm
@@ -12,8 +12,17 @@
 
 ;Iterative Procedure
 
-;;Yet to be completed
+(define (f n)
+  (if (< n 3)
+      n
+      (f-iter 2 1 0 n)))
 
-;(define (f-iter n)
-;  )
+(define (f-iter a b c count)
+  (if (< count 3)
+      a
+      (f-iter (+ a (* 2 b) (* 3 c))
+        a
+        b
+        (- count 1))))
 
+(f 1)
