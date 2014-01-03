bc071548a7530c86b4096d9a85722dc6ec53b04b
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2014-01-03 15:40:24 +0530

    Added Exercise 2.1

---
+++ Chapter2/exercise_2_1.scm
@@ -0,0 +1,19 @@
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (if (< d 0)
+        (cons (/ (* n -1) g) (/ (* d -1) g))
+        (cons (/ n g) (/ d g)))))
+
+(define (numer n)
+  (car n))
+
+(define (denom n)
+  (cdr n))
+
+(print-rat (make-rat -82 -4))
