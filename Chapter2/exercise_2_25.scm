384108838cbef0a3af3acf8800476f6ecd1fdaa0
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2014-01-08 10:48:01 +0530

    Added Exercise 2.25

---
+++ Chapter2/exercise_2_25.scm
@@ -0,0 +1,11 @@
+;Exercise 2.25
+
+(define x (1 3 (5 7) 9))
+(car (cdr (car (cdr (cdr x)))))
+
+(define y (list (list 7)))
+(car (car y))
+
+(define z (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr z))))))))))))
+
