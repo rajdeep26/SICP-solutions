1fb11d0ea9520ce2088affd90474c9545c49f83f
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2014-01-08 10:45:43 +0530

    Added Exercise 2.21

---
+++ Chapter2/exercise_2_21.scm
@@ -0,0 +1,16 @@
+(define (square-list items)
+  (if (null? items)
+    ()
+    (cons (square (car items)) (square-list (cdr items)))))
+
+;;;;;;;;;;;; Second Way;;;;;;;;;;;;
+(define (map proc items)
+  (if (null? items)
+    ()
+    (cons (proc (car items))
+      (map proc (cdr items)))))
+
+(define (square-list items)
+(map square items))
+
+(square-list (list 1 2 3 4 5))
